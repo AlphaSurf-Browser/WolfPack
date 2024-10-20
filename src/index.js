becffbf92538220dff2d542271c4ca7bfce650de
AlphaSurf-Browser <christopherfoxjunior@gmail.com>
2024-10-20 00:55:13 +0000

    Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';      // Import React
+import ReactDOM from 'react-dom'; // Import ReactDOM
+import App from './App';        // Import the main App component
+
+// Render the App component into the root element
+ReactDOM.render(<App />, document.getElementById('root'));
