ec55500b53137401b2b0fa718697a9c3b1c3f87f
AlphaSurf-Browser <christopherfoxjunior@gmail.com>
2024-10-20 03:29:20 +0000

    Commit Server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -14,10 +13,13 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static('public')); // Serve static files from the public directory
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/wolfpack', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/wolfpack', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
@@ -143,4 +145,5 @@ app.post('/api/howls/:id/like', authenticateToken, async (req, res) => {
   }
 });
 
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
