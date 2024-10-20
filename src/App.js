b044c74c6d8bc113ed8e5be568664c70ab4add63
AlphaSurf-Browser <christopherfoxjunior@gmail.com>
2024-10-20 03:16:35 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ const App = () => {
   const [htmlContent, setHtmlContent] = useState('');
 
   useEffect(() => {
-    // Fetch the contents of index.html
-    const fetchHtml = async () => {
-      try {
-        const response = await fetch('/index.html');
-        const text = await response.text();
-        setHtmlContent(text);
-      } catch (error) {
-        console.error('Error fetching index.html:', error);
-      }
-    };
-
-    fetchHtml();
+    // Fetch the content of public/index.html
+    fetch('/index.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text(); // Get the response as text
+      })
+      .then(data => {
+        setHtmlContent(data); // Set the fetched HTML content
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
   }, []);
 
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-    </div>
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
   );
 };
 
